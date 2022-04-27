f6537b228387ddf767ccb7dd328093b498d4745a
Alexrb11 <92717445+Alexrb11@users.noreply.github.com>
2022-04-27 09:49:07 +0200

    Update ejerciciosSql.sql

--- ejerciciosSql.sql
+++ ejerciciosSql.sql
@@ -11,7 +11,7 @@ use northwind;
 
 
 -- pedidos retrasados en 1996.
-
+   
 -- Clientes alemanes que han hecho pedido en julio de 1998.
 
 -- Empleados que tengan entre 18 y 25 años
@@ -33,6 +33,25 @@ use northwind;
 -- Aumenta un 20% el precio de todos los productos que tengan un precio unitario inferior a 10.
 
 
+-- 1.muestra el nombre, precio y coste (precio - 10%) de cada producto.
+-- 2.crea una tabla con las unidades que contiene cada producto, otra columna con las unidades -2 debido a la inflacion y otra con el proveedor que las suministra
+-- 3.Unos clientrs han hecho un pedido desde la misma cuenta, pero lo quieren en paquetes separados, son 5 y el id de pedido es 10250
+
+-- 4.Muestra una tabla con la cantidad de productos que ha pedido cada cliente
+-- 5.Muestra la cantidad de empleados que tiene cada territorio
+-- 6.Una tabla con la cantidad de territorios que hay por region
+
+-- 7. El ejercicio 1 pero solo se mostraran los productos que tengan un coste superior a 10
+-- 8. el ejercicio4 pero , como queremos recompensar a los clientes con un vip, solo necesitamos los que han pedido mas de 80 productos
+-- 9. El nombre de los proveedores que suministran mas de 3 productos.
+
+-- 10. los nombres de los productos en mayusculas
+-- 11. los dos primeros numeros de las id de los productos
+-- 12. los nombres de los territorios en minusculas
+
+-- 13. sumale 5 dias a las fechas de llegada de los pedidos para pedir valoracion al cliente sobre el producto
+-- 14. muestra el dia que se envió el pedido, el dia que llegó y cuanto tiempo transcurrió entre esas dos fechas
+-- 15. una tabla con los años que tenian los empleados cuando les contrataron y que edad tienen ahora
 
 
  
