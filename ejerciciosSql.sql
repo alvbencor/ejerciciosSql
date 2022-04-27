dc8b02b4a2f9499b7cb0cf1dad2d81825e3fe17e
Álvaro Benito <44552582+alvbencor@users.noreply.github.com>
2022-04-27 09:47:21 +0200

    Add files via upload

---
+++ ejerciciosSql.sql
@@ -0,0 +1,45 @@
+use northwind;
+
+
+
+-- Los pedidos enviados a cada ciudad desde Brasil.
+
+-- Los clientes de Barcelona por barrios.
+
+-- los clientes de London.
+
+
+
+-- pedidos retrasados en 1996.
+
+-- Clientes alemanes que han hecho pedido en julio de 1998.
+
+-- Empleados que tengan entre 18 y 25 años
+
+
+
+-- La cantidad de envios que han salido desde portugal
+
+-- Nombre del cliente y el número de pedidos del cliente que mas pedidos ha realizado en la empresa.
+
+-- Nombre del cliente y la cantidad de pedidos del cliente que más pedidos ha hecho en la empresa.
+
+
+
+-- Aumenta el precio de todos los productos un 0.4%
+
+-- Calcula la media del precio de los productos enviados desde Rio de Janeiro
+
+-- Aumenta un 20% el precio de todos los productos que tengan un precio unitario inferior a 10.
+
+
+
+
+ 
+
+
+
+
+
+
+
