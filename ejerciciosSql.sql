532f4652597250836cbf341fef962d8ffa7a676a
Álvaro Benito <44552582+alvbencor@users.noreply.github.com>
2022-04-28 08:43:44 +0200

    Update ejerciciosSql.sql

--- ejerciciosSql.sql
+++ ejerciciosSql.sql
@@ -20,7 +20,7 @@ use northwind;
 
 -- Nombre del cliente y el número de pedidos del cliente que mas pedidos ha realizado en la empresa.
 
--- Nombre del cliente y la cantidad de pedidos del cliente que más pedidos ha hecho en la empresa.
+-- El dia que se envió el pedido, el dia que llegó y cuanto tiempo transcurrió en Francia en 1998.
 
 
 
@@ -35,7 +35,7 @@ use northwind;
 
 -- El nombre de la compañia del cliente, nombre del contacto, código de orden de la compra, decha de la orden de la compra, código de producto y nombre de la compañía proveedora siempre que estas comiencen de por la letra a hasta la letra g. Tienen que haber pedido entre 23 y 187 productos.
 
--- Número de órdenes que se han realizado por años y meses ordenado por año y por mes. 
+-- Número de pedidos que se han realizado por año y meses ordenado por año y por mes. 
 
 
 
